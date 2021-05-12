af252b6bdcdd57d58713d227743a5d8b8fd7d6c1
Manuel Schmidt <manu.schmidt.14@gmail.com>
2021-05-12 21:17:34 +0200

    Added learn() and step() to agent

--- Agent.py
+++ Agent.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+from networks import ActorNetwork, CriticNetwork
+
 """
 For some reason the pytorch function .to(device) takes forever to execute.
 Probably some issue between my cuda version and the pytorch version (0.4.0) that unityagents requires
@@ -45,23 +47,77 @@ class Agent:
         :param buffer_size: size of replay buffer
         :param batch_size: size of learning batch (mini-batch)
         """
-        self.tau = tau
-        self.action_size = action_size
         self.state_size = state_size
-        pass
+        self.action_size = action_size
 
-    def step(self, experience: tuple):
-        pass
+        self.tau = tau
+        self.gamma = gamma
 
-    def act(self, state, add_noise: bool = True):
-        return np.clip(np.random.randn(2, self.action_size), 1, -1)
+        self.actor_local = ActorNetwork(state_size, action_size).to(device)
+        self.actor_target = ActorNetwork(state_size, action_size).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=lr_actor)
+        print(self.actor_local)
+
+        self.critic_local = CriticNetwork(state_size, action_size).to(device)
+        self.critic_target = CriticNetwork(state_size, action_size).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=lr_critic, weight_decay=weight_decay)
+        print(self.critic_local)
+
+        self.hard_update(self.actor_local, self.actor_target)
+        self.hard_update(self.critic_local, self.critic_target)
+
+    def act(self, state, add_noise: bool = False):
+        """ Actor uses the policy to act given a state """
+        state = torch.from_numpy(state).float().to(device)
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local.forward(state).cpu().data.numpy()
+        self.actor_local.train()
+        # if add_noise:
+        #     action += self.noise.sample()
+        return np.clip(action, -1, 1)
 
     def learn(self, experiences):
-        pass
+        states, actions, rewards, next_states, dones = experiences
+
+        # region Update Critic
+        actions_next = self.actor_target.forward(next_states)
+        q_expected = self.critic_local.forward(states, actions)
+        q_targets_next = self.critic_target.forward(next_states, actions_next)
+
+        q_targets = rewards + (self.gamma * q_targets_next * (1 - dones))
+
+        # minimize the loss
+        critic_loss = F.mse_loss(q_expected, q_targets)
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
+        self.critic_optimizer.step()
+        # endregion Update Critic
+
+        # region Update actor
+        # Compute actor loss
+        actions_predictions = self.actor_local.forward(states)
+        actor_loss = -self.critic_local.forward(states, actions_predictions).mean()
+        # Minimize actor loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+        # endregion Update actor
+
+        # region update target network
+        self.soft_update(self.critic_local, self.critic_target)
+        self.soft_update(self.actor_local, self.actor_target)
+        # endregion update target network
 
     def soft_update(self, local_model, target_model):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
+
+    def hard_update(self, local_model, target_model):
+        """Copy the weights and biases from the local to the target network"""
+        for target_param, param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(param.data)
 
 
 class AgentDuo:
@@ -71,20 +127,37 @@ class AgentDuo:
         self.agent1 = agent1
         self.agent2 = agent2
 
-        self.memory = ReplayBuffer(agent1.action_size, buffer_size, batch_size)
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+
+        self.memory = ReplayBuffer(buffer_size, batch_size)
+
+    def step(self, experience1: tuple, experience2: tuple):
+        """
+        Step function for both agents
+        :param experience1: tuple containing the experience of the first agent.
+        :param experience2: tuple containing the experience of the second agent.
+        Each experience is a tuple that looks like the following:
+            (state, action, reward, next_state, done)
+        """
+        self.memory.add(*experience1)
+        self.memory.add(*experience2)
+
+        if len(self.memory) > self.batch_size:
+            self.agent1.learn(self.memory.sample())
+            self.agent2.learn(self.memory.sample())
 
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size):
+    def __init__(self, buffer_size, batch_size):
         """Initialize a ReplayBuffer object.
         Params
         ======
             buffer_size (int): maximum size of buffer
             batch_size (int): size of each training batch
         """
-        self.action_size = action_size
         self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
